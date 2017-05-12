4482898b8f2db462614bd9b27c89d943b2e5d849
Mozaze Sanora Putra <mozazes42@gmail.com>
2017-05-12 16:31:01 +0700

    Push Myclass java
    new update

---
+++ Myclass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ourProject;
+
+/**
+ *
+ * @author mozaze
+ */
+public class Myclass {
+    public static void main(String [] args){
+        System.out.println("Hello Dunia");
+        System.out.println("Coeg");
+    }
+    
+}
