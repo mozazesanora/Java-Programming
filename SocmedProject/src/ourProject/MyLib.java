9b441e94c623fee283f47197287beb79a7555451
Mozaze Sanora Putra <mozazes42@gmail.com>
2017-05-18 09:22:56 +0700

    Library Push
    Jupuk sek men

---
+++ SocmedProject/src/ourProject/MyLib.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ourProject;
+
+/**
+ *
+ * @author mozaze
+ */
+public interface MyLib {
+    public void login(String paramUsername , String paramPassword);
+    public void register(String paramNama , String paramUsername ,String paramJenisKelamin , String paramTelp);
+     
+}
